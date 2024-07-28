8a3a70de34d2db410864e0573617d7fc03ea9d83
Saif M Hakeam <saif.hakeam97@gmail.com>
2024-07-28 16:42:21 +0400

    Added hyperMode to lunch apps

--- karabinerTS/src/index.ts
+++ karabinerTS/src/index.ts
@@ -1,18 +1,34 @@
-import { FromKeyParam, ifApp, layer, map, mapPointingButton, rule, simlayer, ToInputSource, writeToProfile } from "karabiner.ts";
+// Docs: https://karabiner.ts.evanliu.dev/
 
-let karabinerCliPath = "'/Library/Application Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli' karabiner_cli";
+import {
+  FromKeyParam,
+  hyperLayer,
+  ifApp,
+  ifDevice,
+  layer,
+  map,
+  mapPointingButton,
+  rule,
+  ToInputSource,
+  withCondition,
+  writeToProfile,
+} from "karabiner.ts";
 
-let APP_ID_BROWSERS = ["com.google.Chrome", "com.apple.Safari"];
+const karabinerCliPath = "'/Library/Application Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli' karabiner_cli";
 
-let APP_ID_VSCODE = "com.microsoft.VSCode";
+const APP_ID_BROWSERS = ["com.google.Chrome", "com.apple.Safari"];
 
-let inputColemak: ToInputSource = { input_source_id: "com.apple.keylayout.Colemak" };
-let InputAbc: ToInputSource = { input_source_id: "com.apple.keylayout.ABC" };
-let inputArabic: ToInputSource = { input_source_id: "com.apple.keylayout.Arabic" };
+const APP_ID_VSCODE = "com.microsoft.VSCode";
 
-let muteMic = 'osascript -e "set volume input volume 50"; afplay /System/Library/Sounds/Ping.aiff';
-let unmuteMic = 'osascript -e "set volume input volume 0"; afplay /System/Library/Sounds/Pop.aiff';
-let simlayerTimeout = 5000;
+const inputColemak: ToInputSource = { input_source_id: "com.apple.keylayout.Colemak" };
+const InputAbc: ToInputSource = { input_source_id: "com.apple.keylayout.ABC" };
+const inputArabic: ToInputSource = { input_source_id: "com.apple.keylayout.Arabic" };
+
+const muteMic = 'osascript -e "set volume input volume 50"; afplay /System/Library/Sounds/Ping.aiff';
+const unmuteMic = 'osascript -e "set volume input volume 0"; afplay /System/Library/Sounds/Pop.aiff';
+const simlayerTimeout = 5000;
+
+const personalMachine = { product_id: 0x0340, vendor_id: 0x05ac };
 
 // writeToProfile("--dry-run", [
 writeToProfile(
@@ -62,67 +78,80 @@ writeToProfile(
       map("c").to("semicolon", ["command", "option"]),
     ]),
 
-    layer("4", "symbolLayer").manipulators([
-      map("j").to("9", ["shift"]), // (
-      map("k").to("0", ["shift"]), // )
-      map("n").to("open_bracket"), // [
-      map("m").to("close_bracket"), // ]
-      map("u").to("open_bracket", ["shift"]), // {
-      map("i").to("close_bracket", ["shift"]), // }
-      map("semicolon").to("comma", ["shift"]), // <
-      map("period").to("period", ["shift"]), // >
-      map("l").to("1", ["shift"]), // !
-      map("o").to("2", ["shift"]), // @
-      map("h").to("3", ["shift"]), // #
-      map("semicolon").to("4", ["shift"]), // $
-      map("quote").to("grave_accent_and_tilde"), // `
-      map("p").to("5", ["shift"]), // %
-      map("b").to("7", ["shift"]), // &
-      map("y").to("8", ["shift"]), // *
-      map("spacebar").to("hyphen", ["shift"]), // _
-    ]),
+    layer("4", "symbolLayer")
+      .configKey((v) => v.toIfAlone("4"), true)
+      .manipulators([
+        map("j").to("9", ["shift"]), // (
+        map("k").to("0", ["shift"]), // )
+        map("n").to("open_bracket"), // [
+        map("m").to("close_bracket"), // ]
+        map("u").to("open_bracket", ["shift"]), // {
+        map("i").to("close_bracket", ["shift"]), // }
+        map("semicolon").to("comma", ["shift"]), // <
+        map("period").to("period", ["shift"]), // >
+        map("l").to("1", ["shift"]), // !
+        map("o").to("2", ["shift"]), // @
+        map("h").to("3", ["shift"]), // #
+        map("semicolon").to("4", ["shift"]), // $
+        map("quote").to("grave_accent_and_tilde"), // `
+        map("p").to("5", ["shift"]), // %
+        map("b").to("7", ["shift"]), // &
+        map("y").to("8", ["shift"]), // *
+        map("spacebar").to("hyphen", ["shift"]), // _
+      ]),
 
-    layer("0", "MediaLayer").manipulators([
-      map("e").to("volume_up"),
-      map("d").to("volume_down"),
-      map("s").to("vk_consumer_previous"),
-      map("f").to("vk_consumer_next"),
-      map("a").to("vk_consumer_play"),
-      map("g").to("a", ["option", "shift"]), // to toggle dark reader on / off in chrome
-      map("r").to("vk_consumer_brightness_up"),
-      map("w").to("vk_consumer_brightness_down"),
-      map("t").to("up_arrow", ["control"]),
-      map("v").to("down_arrow", ["control"]),
-      map("y").toInputSource(inputColemak),
-      map("h").toInputSource(inputArabic),
+    layer("0", "MediaLayer")
+      .configKey((v) => v.toIfAlone("0"), true)
+      .manipulators([
+        map("e").to("volume_up"),
+        map("d").to("volume_down"),
+        map("s").to("vk_consumer_previous"),
+        map("f").to("vk_consumer_next"),
+        map("a").to("vk_consumer_play"),
+        map("g").to("a", ["option", "shift"]), // to toggle dark reader on / off in chrome
+        map("r").to("vk_consumer_brightness_up"),
+        map("w").to("vk_consumer_brightness_down"),
+        map("t").to("up_arrow", ["control"]),
+        map("v").to("down_arrow", ["control"]),
+        map("y").toInputSource(inputColemak),
+        map("h").toInputSource(inputArabic),
 
-      // Mute & unmute mic
-      map("1").to$(muteMic),
-      map("q").to$(unmuteMic),
-    ]),
+        // Mute & unmute mic
+        map("1").to$(muteMic),
+        map("q").to$(unmuteMic),
+      ]),
 
-    layer("2", "NumLayer").manipulators([
-      map("u").to("7"),
-      map("i").to("8"),
-      map("o").to("9"),
-      map("j").to("4"),
-      map("k").to("5"),
-      map("l").to("6"),
-      map("n").to("1"),
-      map("m").to("2"),
-      map("comma").to("3"),
-      map("spacebar").to("0"),
-    ]), 
+    layer("2", "NumLayer")
+      .configKey((v) => v.toIfAlone("2"), true)
+      .manipulators([
+        map("u").to("7"),
+        map("i").to("8"),
+        map("o").to("9"),
+        map("j").to("4"),
+        map("k").to("5"),
+        map("l").to("6"),
+        map("n").to("1"),
+        map("m").to("2"),
+        map("comma").to("3"),
+        map("spacebar").to("0"),
+      ]),
 
-    simlayer("-", undefined, simlayerTimeout).manipulators([
-      map("c").to$(openApp("com.google.Chrome")),
-      map("v").to$(openApp("com.microsoft.VSCode")),
-      map("t").to$(openApp("com.microsoft.teams")),
-      map("e").to$(openApp("com.apple.finder")),
-      map("a").to$(openApp("com.google.android.studio")),
-      map("x").to$(openApp("com.apple.dt.Xcode")),
-      map("s").to$(openApp("com.apple.iphonesimulator")),
-    ]),
+    hyperLayer("a")
+      .description("Open App Mode")
+      .leaderMode()
+      .leaderMode({ sticky: false, escape: ["escape", "spacebar"] })
+      .notification() // Notification is highly recommanded when use leader mode
+      .manipulators([
+        withCondition(ifDevice(personalMachine).unless())([
+          map("t").to$(openApp("com.microsoft.teams")),
+          map("g").to$(openApp("com.google.android.studio")),
+          map("x").to$(openApp("com.apple.dt.Xcode")),
+          map("s").to$(openApp("com.apple.iphonesimulator")),
+        ]),
+        map("c").to$(openApp("com.google.Chrome")),
+        map("v").to$(openApp("com.microsoft.VSCode")),
+        map("e").to$(openApp("com.apple.finder")),
+      ]),
 
     rule("General").manipulators([
       map("0", ["control", "option"])
@@ -136,6 +165,7 @@ writeToProfile(
 
       mapWithAnyMod("3").to("left_shift").toIfAlone("a", ["command", "shift"]),
       mapWithAnyMod("9").to("right_shift").toIfAlone("return_or_enter"),
+      map("-").toHyper().toIfAlone("-"),
 
       mapWithAnyMod("right_command").toIfAlone("escape").toIfHeldDown("right_command").condition(ifApp(APP_ID_BROWSERS)),
       mapWithAnyMod("right_command").toIfAlone("f15").toIfHeldDown("right_command"),
@@ -152,18 +182,18 @@ writeToProfile(
     ]),
 
     rule("Mouse configs 123").manipulators([
-      //;; Switch back & forward buttons in mouse.
+      // Switch back & forward buttons in mouse.
       mapPointingButton("button4").toPointingButton("button5"),
       mapPointingButton("button5").toPointingButton("button4"),
 
-      //;; Switch between workspaces.
+      // Switch between workspaces.
       mapPointingButton("button4", "left_control").to("left_arrow", ["control"]),
       mapPointingButton("button5", "left_control").to("right_arrow", ["control"]),
 
-      //;;  open expose mode in macos
+      //  open expose mode in macos
       mapPointingButton("button5", "left_command").to("up_arrow", ["control"]),
       mapPointingButton("button4", "left_command").to("down_arrow", ["control"]),
-      //;;  Mute & unmute mic
+      //  Mute & unmute mic
       mapPointingButton("button5", "left_shift").to$(muteMic),
       mapPointingButton("button4", "left_shift").to$(unmuteMic),
     ]),
