23f9d65eb111503db7e06e2dc759d158cfd60eb0
Saif M Hakeam <saif.hakeam97@gmail.com>
2024-12-29 13:58:26 +0300

    generic stuff

--- karabinerTS/src/index.ts
+++ karabinerTS/src/index.ts
@@ -34,6 +34,37 @@ const personalMachine = { product_id: 0x0340, vendor_id: 0x05ac };
 writeToProfile(
   "Goku",
   [
+    rule("General").manipulators([
+      map("w", "left_command").to("left_command", ["option"]).description("Amethyst Mode"),
+      map("0", ["control", "option"])
+        .toInputSource(InputAbc)
+        .to$(chainCmds([switchProfile("stock"), getCmdToPlayChime(1)])),
+      map("caps_lock").to("left_command"),
+
+      // To prevent accidentally hiding windows & minimizing them. Mac doesn't have a way to disable this.
+      map("m", "command").to("m", ["command", "shift"]),
+      map("h", "command").to("m", ["command", "shift"]),
+
+      mapWithAnyMod("3").to("left_shift").toIfAlone("a", ["command", "shift"]),
+      mapWithAnyMod("9").to("right_shift").toIfAlone("return_or_enter"),
+      map("-").toHyper().toIfAlone("-"),
+
+      mapWithAnyMod("right_command").toIfAlone("escape").toIfHeldDown("right_command").condition(ifApp(APP_ID_BROWSERS)),
+      mapWithAnyMod("right_command").toIfAlone("f15").toIfHeldDown("right_command"),
+      mapWithAnyMod("right_option").to("right_option", ["left_command"]).toIfAlone("f14").condition(ifApp(APP_ID_VSCODE)),
+      mapWithAnyMod("right_option").to("right_option").toIfAlone("f14"),
+
+      // in finder make enter open files
+      map("return_or_enter").to("semicolon", ["command"]).condition(ifApp("com.apple.finder")),
+
+      mapWithAnyMod("tab").toIfHeldDown("left_control").toIfAlone("tab"),
+      mapWithAnyMod("right_shift").to("left_command", ["option", "shift"]).toIfAlone("f18"),
+
+      map("1").to("left_command", ["option"]).toIfAlone("f17"), // Amethyst Mode
+
+      withCondition(ifDevice(personalMachine).unless(), ifApp("com.raycast.macos"))([map("v", "command").toMeh()]),
+    ]),
+
     layer("left⌘", "VimLayer").manipulators([
       // Indent & outdent
       map("q").to("tab", "shift"),
@@ -136,52 +167,21 @@ writeToProfile(
         map("spacebar").to("0"),
       ]),
 
-    duoLayer("right⌘", "l").manipulators([
-      // Personal machine specific
-      withCondition(ifDevice(personalMachine))([map("f").to$(openApp("com.TickTick.task.mac"))]),
+    //duoLayer("right⌘", "l").manipulators([
+    //// Personal machine specific
+    //withCondition(ifDevice(personalMachine))([map("f").to$(openApp("com.TickTick.task.mac"))]),
 
-      // Work machine specific
-      withCondition(ifDevice(personalMachine).unless())([
-        map("f").to$(openApp("com.microsoft.teams2")),
-        map("g").to$(openApp("com.google.android.studio")),
-        map("x").to$(openApp("com.apple.dt.Xcode")),
-        map("d").to$(openApp("com.apple.iphonesimulator")),
-      ]),
-      map("c").to$(openApp("com.google.Chrome")),
-      map("v").to$(openApp("com.todesktop.230313mzl4w4u92")), // Cursor AI
-      map("e").to$(openApp("com.apple.finder")),
-    ]),
-
-    rule("General").manipulators([
-      map("0", ["control", "option"])
-        .toInputSource(InputAbc)
-        .to$(chainCmds([switchProfile("stock"), getCmdToPlayChime(1)])),
-      map("caps_lock").to("left_command"),
-
-      // To prevent accidentally hiding windows & minimizing them. Mac doesn't have a way to disable this.
-      map("m", "command").to("m", ["command", "shift"]),
-      map("h", "command").to("m", ["command", "shift"]),
-
-      mapWithAnyMod("3").to("left_shift").toIfAlone("a", ["command", "shift"]),
-      mapWithAnyMod("9").to("right_shift").toIfAlone("return_or_enter"),
-      map("-").toHyper().toIfAlone("-"),
-
-      mapWithAnyMod("right_command").toIfAlone("escape").toIfHeldDown("right_command").condition(ifApp(APP_ID_BROWSERS)),
-      mapWithAnyMod("right_command").toIfAlone("f15").toIfHeldDown("right_command"),
-      mapWithAnyMod("right_option").to("right_option", ["left_command"]).toIfAlone("f14").condition(ifApp(APP_ID_VSCODE)),
-      mapWithAnyMod("right_option").to("right_option").toIfAlone("f14"),
-
-      // in finder make enter open files
-      map("return_or_enter").to("semicolon", ["command"]).condition(ifApp("com.apple.finder")),
-
-      mapWithAnyMod("tab").toIfHeldDown("left_control").toIfAlone("tab"),
-      mapWithAnyMod("right_shift").to("left_command", ["option", "shift"]).toIfAlone("f18"),
-
-      map("1").to("left_command", ["option"]).toIfAlone("f17"), // Amethyst Mode
-
-      withCondition(ifDevice(personalMachine).unless(), ifApp("com.raycast.macos"))([map("v", "command").toMeh()]),
-    ]),
-
+    //// Work machine specific
+    //withCondition(ifDevice(personalMachine).unless())([
+    //map("f").to$(openApp("com.microsoft.teams2")),
+    //map("g").to$(openApp("com.google.android.studio")),
+    //map("x").to$(openApp("com.apple.dt.Xcode")),
+    //map("d").to$(openApp("com.apple.iphonesimulator")),
+    //]),
+    //map("c").to$(openApp("com.google.Chrome")),
+    //map("v").to$(openApp("com.todesktop.230313mzl4w4u92")), // Cursor AI
+    //map("e").to$(openApp("com.apple.finder")),
+    //]),
     rule("Mouse configs 123").manipulators([
       // Switch back & forward buttons in mouse.
       mapPointingButton("button4").toPointingButton("button5"),
