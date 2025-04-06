223ced7942ee6e471108dbc07407bc484886f2ee
Saif M Hakeam <saif.hakeam97@gmail.com>
2025-04-06 23:21:56 +0400

    add window border color

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -3,6 +3,6 @@ return {
     name = "catppuccin", priority = 1000,
     config = function()
         vim.cmd.colorscheme "catppuccin"
+        vim.api.nvim_set_hl(0, 'WinSeparator', { fg = '#6e738d' })
     end
-
 }
--- nvim/lua/vimOptions.lua
+++ nvim/lua/vimOptions.lua
@@ -85,4 +85,3 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
