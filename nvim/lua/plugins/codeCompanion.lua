a2437bd7b6b7fbeee01edc030b2b01103fb9e411
Saif M Hakeam <saif.hakeam97@gmail.com>
2025-04-19 14:31:31 +0400

    Working code companion with copilot

--- nvim/lua/plugins/codeCompanion.lua
+++ nvim/lua/plugins/codeCompanion.lua
@@ -1,93 +1,41 @@
 require("config.codeCompanionConfigs")
 
 return {
-	"olimorris/codecompanion.nvim",
-	config = true,
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-treesitter/nvim-treesitter",
-	},
-
-	opts = {
-		adapters = adapter_lib,
-		prompt_library = prompt_lib,
-		strategies = {
-			chat = {
-				adapter = "copilot",
-				roles = {
-					user = "olimorris",
-				},
-				keymaps = {
-					send = {
-						modes = {
-							i = { "<C-CR>", "<C-s>" },
-						},
-					},
-					completion = {
-						modes = {
-							i = "<C-x>",
-						},
-					},
-				},
-				slash_commands = {
-					["buffer"] = {
-						opts = {
-							provider = "snacks",
-							keymaps = {
-								modes = {
-									i = "<C-b>",
-								},
-							},
-						},
-					},
-					["help"] = {
-						opts = {
-							provider = "snacks",
-							max_lines = 1000,
-						},
-					},
-					["file"] = {
-						opts = {
-							provider = "snacks",
-						},
-					},
-					["symbols"] = {
-						opts = {
-							provider = "snacks",
-						},
-					},
-				},
-				tools = {
-					vectorcode = {
-						description = "Run VectorCode to retrieve the project context.",
-						callback = function()
-							return require("vectorcode.integrations").codecompanion.chat.make_tool()
-						end,
-					},
-				},
-			},
-			inline = { adapter = "copilot" },
-		},
-		display = {
-			action_palette = {
-				provider = "default",
-			},
-			chat = {
-				-- show_references = true,
-				-- show_header_separator = false,
-				-- show_settings = false,
-			},
-			diff = {
-				enabled = true,
-				close_chat_at = 240, -- Close an open chat buffer if the total columns of your display are less than...
-				layout = "vertical", -- vertical|horizontal split for default provider
-				opts = { "internal", "filler", "closeoff", "algorithm:patience", "followwrap", "linematch:120" },
-				provider = "mini_diff", -- default|mini_diff
-			},
-		},
-		opts = {
-			log_level = "DEBUG",
-		},
-	},
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+      { "nvim-lua/plenary.nvim" },
+      -- Test with blink.cmp
+      -- {
+      --   "saghen/blink.cmp",
+      --   lazy = false,
+      --   version = "*",
+      --   opts = {
+      --     keymap = {
+      --       preset = "enter",
+      --       ["<S-Tab>"] = { "select_prev", "fallback" },
+      --       ["<Tab>"] = { "select_next", "fallback" },
+      --     },
+      --     cmdline = { sources = { "cmdline" } },
+      --     sources = {
+      --       default = { "lsp", "path", "buffer", "codecompanion" },
+      --     },
+      --   },
+      -- },
+      -- Test with nvim-cmp
+      -- { "hrsh7th/nvim-cmp" },
+    },
+    opts = {
+      --Refer to: https://github.com/olimorris/codecompanion.nvim/blob/main/lua/codecompanion/config.lua
+      strategies = {
+        --NOTE: Change the adapter as required
+        chat = { adapter = "copilot" },
+        inline = { adapter = "copilot" },
+      },
+      opts = {
+        log_level = "DEBUG",
+      },
+    },
+  },
 }
-
