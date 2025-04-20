21355c0fc918777d815a9c3bcff89c3eaf74b27e
Saif M Hakeam <saif.hakeam97@gmail.com>
2025-04-21 00:17:20 +0400

    change to lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,47 +1,59 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    local custom_gruvbox = require("lualine.themes.catppuccin")
-    custom_gruvbox.inactive.a.bg = "#8bd5ca"
-    custom_gruvbox.inactive.a.fg = "#181926"
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local custom_gruvbox = require("lualine.themes.catppuccin")
+		custom_gruvbox.inactive.a.bg = "#8bd5ca"
+		custom_gruvbox.inactive.a.fg = "#181926"
 
-    require("lualine").setup({
-      options = {
-        theme = custom_gruvbox,
-        globalstatus = false,
-      },
-      sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = {
-          {
-            function()
-              return vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
-                  .. "->"
-                  .. vim.fn.expand('%:t')
-            end,
-            icon = "󱧽", -- Customize the icon if needed
-            color = { fg = "#8aadf4" },
-          },
-          {
-            "filename",
-            path = 1,
-          },
-        },
-        lualine_x = { "encoding", "fileformat", "filetype" },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
-      },
+		require("lualine").setup({
+			options = {
+				theme = custom_gruvbox,
+				globalstatus = false,
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = {
+					{
+						function()
+							return vim.fn.fnamemodify(vim.fn.getcwd(), ":t") .. "->" .. vim.fn.expand("%:t")
+						end,
+						icon = "󱧽", -- Customize the icon if needed
+						color = { fg = "#8aadf4" },
+					},
+					{
+						"filename",
+						path = 1,
+					},
+				},
+				lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
 
-      -- inactive_winbar = {
-      -- 	lualine_a = { { "filename", color = { fg = "#232634" } } }, -- customize font and background color
-      -- 	lualine_b = {},
-      -- 	lualine_c = {},
-      -- 	lualine_x = {},
-      -- 	lualine_y = {},
-      -- 	lualine_z = {},
-      -- },
-    })
-  end,
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = {
+					{
+						"filename",
+						color = { fg = "#f2cdcd"}, -- Set the text color (fg) and background color (bg)
+					},
+				},
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = {},
+			},
+
+			-- inactive_winbar = {
+			-- 	lualine_a = { { "filename", color = { fg = "#232634" } } }, -- customize font and background color
+			-- 	lualine_b = {},
+			-- 	lualine_c = {},
+			-- 	lualine_x = {},
+			-- 	lualine_y = {},
+			-- 	lualine_z = {},
+			-- },
+		})
+	end,
 }
