33e01a94974a39cb91e25a332f8680d6cfd9e244
Saif M Hakeam <saif.hakeam97@gmail.com>
2025-05-03 01:56:36 +0400

    added keymaps to neo-tree

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,40 +1,70 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-    -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
-  },
-  lazy = false, -- neo-tree will lazily load itself
-  ---@module "neo-tree"
-  ---@type neotree.Config?
-  opts = {
-    window = {
-      mappings = {
-        ["<BS>"] = "", -- Disables backspace navigation
-        -- Map 'u' to move up
-        -- ["u"] = "navigate_up", -- Moves the cursor up in the tree
-        ["n"] = "close_node",
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+		-- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+	},
+	lazy = false, -- neo-tree will lazily load itself
+	---@module "neo-tree"
+	---@type neotree.Config?
+	opts = {
+		window = {
+			mappings = {
+				["<BS>"] = "", -- Disables backspace navigation
+				-- Map 'u' to move up
+				-- ["u"] = "navigate_up", -- Moves the cursor up in the tree
+				["n"] = "close_node",
 
-        -- Map 'e' to move down
-        ["e"] = "navigate_down", -- Moves the cursor down in the tree
+				-- Map 'e' to move down
+				["e"] = "", -- Moves the cursor down in the tree
+				["d"] = "toggle_auto_expand_width",
 
-        -- Map 'i' to go inside (expand directory or open file)
-        ["i"] = "open",         -- Expands a directory or opens a file
-        ["<Esc>"] = "close_window", -- Closes Neo-tree when pressing Escape twice
-        ["f"] = "",
-      },
-    },
-    filesystem = {
-      follow_current_file = true,          -- Optional: Automatically focus the current file
-      hijack_netrw_behavior = "open_current", -- Optional: Adjusts netrw behavior
-      filtered_items = {
-        visible = true,                    -- This will show hidden items by default
-        hide_dotfiles = false,             -- This will show dotfiles
-        hide_gitignored = false,           -- This will show gitignored files
-      },
-    },
-  },
+				-- Map 'i' to go inside (expand directory or open file)
+				["i"] = "open",         -- Expands a directory or opens a file
+				["<Esc>"] = "close_window", -- Closes Neo-tree when pressing Escape twice
+
+				["<leader>pn"] = {
+					desc = "Copy [p]ath but [f]ilename only",
+					function(state)
+						local node = state.tree:get_node()
+						local filename = node.name
+						vim.fn.setreg("+", filename)
+						vim.notify("Copied: " .. filename)
+					end,
+				},
+				["<leader>pf"] = {
+					desc = "Copy [p]ath [f]ully for the selected file",
+					function(state)
+						local node = state.tree:get_node()
+						local filename = node:get_id()
+						vim.fn.setreg("+", filename)
+						vim.notify("Copied: " .. filename)
+					end,
+				},
+				["<leader>od"] = {
+					desc = "[o]pen [d]irectory",
+					function(state)
+						local node = state.tree:get_node()
+						local filepath = node:get_id()
+						filepath = vim.fn.fnamemodify(filepath, ":h")
+						vim.fn.system("open " .. filepath)
+						vim.notify("opening: " .. filepath)
+					end,
+				},
+				["<space>"] = "",
+			},
+		},
+		filesystem = {
+			follow_current_file = true,          -- Optional: Automatically focus the current file
+			hijack_netrw_behavior = "open_current", -- Optional: Adjusts netrw behavior
+			filtered_items = {
+				visible = true,                    -- This will show hidden items by default
+				hide_dotfiles = false,             -- This will show dotfiles
+				hide_gitignored = false,           -- This will show gitignored files
+			},
+		},
+	},
 }
