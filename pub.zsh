4777a1a2335b37e0f5e6a1d1921e4de4d3a47dfa
Saif M Hakeam <saif.hakeam97@gmail.com>
2025-05-08 21:54:56 +0400

    introduce pub.zsh

---
+++ pub.zsh
@@ -0,0 +1,12 @@
+
+alias l='lsd -ali'
+alias cat='bat'
+alias cd='z'
+alias ci='zi'
+alias nv='nvim'
+alias python='python3'
+alias py='python3'
+alias pysour='source ./venv/bin/activate'
+alias pip='pip3'
+alias v='nvim'
+
