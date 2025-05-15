70d6dbaae35b348f59493db296fd2de1918610dc
Saif M Hakeam <saif.hakeam97@gmail.com>
2025-05-16 00:48:56 +0400

    Added syntax highlighting & cmp to terminal

--- cmds.sh
+++ cmds.sh
@@ -2,3 +2,5 @@
 ln -s $HOME/dotfiles/lvim $HOME/.config/lvim
 ln -s $HOME/dotfiles/ghostty $HOME/.config/ghostty
 ln -s $HOME/dotfiles/lazygit $HOME/.config/lazygit
+
+brew install zsh-autosuggestions zsh-syntax-highlighting
--- pub.zsh
+++ pub.zsh
@@ -14,3 +14,15 @@ alias pip='pip3'
 alias v='nvim'
 
 export XDG_CONFIG_HOME="$HOME/.config"
+
+# Locale settings
+export LANG="en_US.UTF-8" # Sets default locale for all categories
+export LC_ALL="en_US.UTF-8" # Overrides all other locale settings
+export LC_CTYPE="en_US.UTF-8" # Controls character classification and case conversion
+
+# Use Neovim as default editor
+export EDITOR="nvim"
+export VISUAL="nvim"
+
+source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
