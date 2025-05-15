bf4757e06d727f891fd8ab76ae5ad624f6b1b412
Saif M Hakeam <saif.hakeam97@gmail.com>
2025-05-16 01:58:50 +0400

    disable spelling in terminals

--- nvim/lua/autoCmd.lua
+++ nvim/lua/autoCmd.lua
@@ -18,10 +18,16 @@ else
 	})
 
 	vim.api.nvim_create_autocmd("FileType", {
-		pattern = { "Avante*"}, 
+		pattern = { "Avante*" },
 		callback = function()
 			vim.api.nvim_win_set_option(0, "winfixwidth", false)
 			vim.api.nvim_win_set_option(0, "winfixheight", true)
+		end,
+	})
+	vim.api.nvim_create_autocmd("TermOpen", {
+		pattern = "*",
+		callback = function()
+			vim.wo.spell = false
 		end,
 	})
 end
