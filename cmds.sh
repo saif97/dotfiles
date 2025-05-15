ab36174a15e96a876e7e83bd6c2a219bcc5879ad
Saif M Hakeam <saif.hakeam97@gmail.com>
2025-05-15 23:48:53 +0400

    Added startship rs

--- cmds.sh
+++ cmds.sh
@@ -1,3 +1,4 @@
 # linking vim config
 ln -s $HOME/dotfiles/lvim $HOME/.config/lvim
 ln -s $HOME/dotfiles/ghostty $HOME/.config/ghostty
+ln -s $HOME/dotfiles/lazygit $HOME/.config/lazygit
--- pub.zsh
+++ pub.zsh
@@ -1,7 +1,10 @@
+export STARSHIP_CONFIG=~/dotfiles/starship/starship.toml
+export STARSHIP_CACHE=~/.starship/cache
+eval "$(starship init zsh)"
 
 alias l='lsd -ali'
 alias cat='bat'
-alias cd='z'
+# alias cd='z'
 alias ci='zi'
 alias nv='nvim'
 alias python='python3'
@@ -10,3 +13,4 @@ alias pysour='source ./venv/bin/activate'
 alias pip='pip3'
 alias v='nvim'
 
+export XDG_CONFIG_HOME="$HOME/.config"
