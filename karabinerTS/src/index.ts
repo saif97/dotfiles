7f5512587b9b971ce26a1d55de22ed284d3db3bc
saif.hakeam <saif.hakeam97@gmail.com>
2025-11-02 12:01:06 +0400

    Fix issue with brightness down key

--- karabinerTS/src/index.ts
+++ karabinerTS/src/index.ts
@@ -47,7 +47,7 @@ writeToProfile(
 		duoLayer("left⌘", "e", "amethyst_throw_mode").manipulators(rightLetterPad.flat().map((key) => map(key).to(key, ["shift", "control"]))),
 
 		layer("f1", "desktop_switch_mode")
-			.configKey((v) => v.toIfAlone("f1"), true)
+			.configKey((v) => v.toIfAlone("vk_consumer_brightness_down"), true)
 			.manipulators([
 			// u  i  o  p     →  Desktop 9, 10, 11, 12
 			// j  k  l  ;     →  Desktop 5, 6, 7, 8
@@ -228,7 +228,8 @@ writeToProfile(
 			mapWithAnyMod("tab").toIfHeldDown("left_control").toIfAlone("tab"),
 			map("l", ["control"]).to("tab", ["control"]).condition(ifApp(APP_ID_FIREFOX)),
 			map("j", ["control"]).to("tab", ["control", "shift"]).condition(ifApp(APP_ID_FIREFOX)),
-			mapWithAnyMod("right_shift").to("left_command", ["option", "shift"]).toIfAlone("f18"),
+			mapWithAnyMod("right_shift").toIfAlone("f18"),
+			// map("v", "right_shift").to("f18").to("4"),
 
 			map("1").to("left_command", ["option"]).toIfAlone("f17"), // Amethyst Mode
 
