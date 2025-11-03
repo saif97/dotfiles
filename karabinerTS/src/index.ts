4fb7719ed7128d9be0bf531a606736f8e80a3597
saif.hakeam <saif.hakeam97@gmail.com>
2025-11-03 23:32:53 +0400

    Added clipboard history via spotlight support

--- karabinerTS/src/index.ts
+++ karabinerTS/src/index.ts
@@ -229,7 +229,7 @@ writeToProfile(
 			map("l", ["control"]).to("tab", ["control"]).condition(ifApp(APP_ID_FIREFOX)),
 			map("j", ["control"]).to("tab", ["control", "shift"]).condition(ifApp(APP_ID_FIREFOX)),
 			mapWithAnyMod("right_shift").to("left_command", ["option", "shift"]).toIfAlone("f18"),
-			// map("v", "right_shift").to("f18").to("4"),
+			map("v", ["left_command", "option", "shift"]).to([{ key_code: "f18" }, { key_code: "4", modifiers: ["command"] }]),
 
 			map("1").to("left_command", ["option"]).toIfAlone("f17"), // Amethyst Mode
 
