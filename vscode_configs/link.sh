dd9741cfa60e6b6e59c01ae875a2e27629d631c8
saif.hakeam <saif.hakeam97@gmail.com>
2025-12-03 21:51:15 +0300

    sync vscode stuff

---
+++ vscode_configs/link.sh
@@ -0,0 +1,2 @@
+ln -s $DOT_FILES/vscode_configs/settings.json "${HOME}/Library/Application Support/Antigravity/User/settings.json"
+ln -s $DOT_FILES/vscode_configs/keybindings.json "${HOME}/Library/Application Support/Antigravity/User/keybindings.json"
