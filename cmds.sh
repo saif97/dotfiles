1bb96ba1d4634858a9e85aab97e47629b8a2ecff
saif.hakeam <saif.hakeam97@gmail.com>
2025-12-05 18:16:15 +0300

    add lazy docker & its configs file

--- cmds.sh
+++ cmds.sh
@@ -3,5 +3,6 @@ ln -s $HOME/dotfiles/ghostty $HOME/.config/ghostty
 ln -s $HOME/dotfiles/aiSystemInstructions.md $HOME/.gemini/GEMINI.md
 ln -s $HOME/dotfiles/amethyst $HOME/.config/amethyst
 ln -s $HOME/dotfiles/lazygit $HOME/.config/lazygit
+ln -s $HOME/dotfiles/lazydocker $HOME/.config/lazydocker
 
 git config --global core.excludesfile ~/dotfiles/global.gitignore
