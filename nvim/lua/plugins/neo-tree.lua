81f1547dfee69c587d40251da68d69fd3b9d7d78
saif.hakeam <saif.hakeam97@gmail.com>
2025-12-29 21:53:12 +0400

    enable symlink in neo-tree

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -11,6 +11,11 @@ return {
 	---@module "neo-tree"
 	---@type neotree.Config?
 	opts = {
+		default_component_configs = {
+			symlink_target = {
+				enabled = true,
+			},
+		},
 		window = {
 			mappings = {
 				["<BS>"] = "", -- Disables backspace navigation
