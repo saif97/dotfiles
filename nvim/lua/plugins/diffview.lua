16105a75c50b7f25cbeffb07dbd988b7103135f5
Saif <saif.hakeam97@gmail.com>
2026-08-02 22:11:11 +0000

    add doubt.nvim

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -1,7 +1,18 @@
 return {
-	"https://github.com/sindrets/diffview.nvim",
-	cmd = { "DiffviewOpen", "DiffviewFileHistory", "DiffviewClose", "DiffviewToggleFiles", "DiffviewRefresh" },
-	opts = {
-		enhanced_diff_hl = true,
+	{
+		"https://github.com/sindrets/diffview.nvim",
+		cmd = { "DiffviewOpen", "DiffviewFileHistory", "DiffviewClose", "DiffviewToggleFiles", "DiffviewRefresh" },
+		opts = {
+			enhanced_diff_hl = true,
+		},
+	},
+	{
+		"makefinks/doubt.nvim",
+		enabled = isPersonalMachine(),
+		commit = "4f474018a06db31cb40d4577876662ac931d537f",
+		dependencies = { "MunifTanjim/nui.nvim" },
+		config = function()
+			require("doubt").setup()
+		end,
 	},
 }
